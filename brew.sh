ee4b08be104ed4538781df14635b6a08ccca94fe
Joakim Olsson <joakim@unbound.se>
2018-03-14 18:38:15 +0100

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we're using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+for tap in $(cat brew-taps)
+do
+  brew tap ${tap}
+done
+
+brew install $(< brew-packages)
+
+brew cask install $(< brew-cask-packages)
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+brew link --force gettext
+
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Passwordless sudo
+echo "$(whoami)            ALL = (ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$(whoami)"
+
+# Install applications
+./brew.sh
+./zsh.sh
+./macos.sh gwaihir
+
+# Symlink dot-files
+for file in .*; do
+	[ -r "$file" ] && [ -f "$file" ] && ln -fs "$(pwd {BASH_SOURCE[0]})/$file" ~/$file;
+done
+
+# Post installation
+echo "Load default color schema in iTerm2"
+
+echo "Reboot :)"
